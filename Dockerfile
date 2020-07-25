7d9926a94bf4788e87481587856bc7ab0b535c4f
Alex Ostapiuk <alexostapiuk00@gmail.com>
2020-07-25 09:24:17 +0300

    fix Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -2,8 +2,10 @@ FROM node:alpine
 
 WORKDIR /usr/app
 
-COPY package*.json ./
+COPY package.json ./
 RUN npm install
 COPY . .
+
+EXPOSE 5000
 
 CMD ["npm", "start"]
--- config/db.js
+++ config/db.js
@@ -3,19 +3,9 @@ const mongoose = require('mongoose');
 const configService = require('../utils/configService');
 
 const connectDB = async () => {
-  console.log(process.env);
-  console.log('MONGO-URL', await configService.getSecret('MONGO-URL'));
-  console.log('MONGO-URL', await configService.getSecret('MONGO-URL'));
-  console.log('MONGO_URL', await configService.getSecret('MONGO_URL'));
   const db = await configService.getSecret('MONGO_URL');
   try {
-    await mongoose.connect(db, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
-      useFindAndModify: false,
-    });
-    console.log('MongoDB Connected...');
+    await mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => console.log('MongoDB Connected...'));
   } catch (err) {
     console.error(err);
     process.exit(1);
